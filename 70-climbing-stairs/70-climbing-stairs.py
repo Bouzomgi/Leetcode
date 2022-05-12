be0eec91c978d31ee7b005dfa351af3c76a8072f
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-12 00:52:05 -0400

    Time: 50 ms (25.99%), Space: 13.8 MB (58.70%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,12 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-          #naive recursive approach
+         #naive recursive approach
 #         if n <= 2:
 #             return n
 #         return self.climbStairs(n-1) + self.climbStairs(n-2)
     
-          #memoization
+    
+         #memoization
 #         cache = {}
 #         cache[1] = 1
 #         cache[2] = 2
@@ -18,12 +19,26 @@ class Solution:
         
 #         return memo(n)
 
+
         #memoization using functools @cache
-        @lru_cache
-        def memo(i):
-            if i <= 2:
-                return i
-            return memo(i-1) + memo(i-2)
+#         @lru_cache
+#         def memo(i):
+#             if i <= 2:
+#                 return i
+#             return memo(i-1) + memo(i-2)
         
-        return memo(n)
+#         return memo(n)
+
+#tabulation
+        if n<=2:
+            return n
+        
+        tab = [0] * (n+1) 
+        tab[1] = 1
+        tab[2] = 2
+        for i in range(3, n+1):
+            tab[i] = tab[i-1] + tab[i-2]
+            
+        return tab[-1]
+        
     
