4378227fb9c6b6985cbbfdee9a4bb7a01c915722
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-12 21:02:54 -0400

    Time: 48 ms (37.23%), Space: 13.8 MB (67.97%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -2,12 +2,26 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         
         #naive recursion
-        @cache
+#         def memo(i):
+#             if i == 0:
+#                 return nums[0]
+#             elif i == 1:
+#                 return max(nums[1], nums[0])
+#             return max(nums[i] + memo(i-2), memo(i-1))
+        
+#         return memo(len(nums)-1)
+    
+        #memoization
+        if len(nums) == 1: 
+            return nums[0]
+        
+        cache = {}
+        cache[0] = nums[0]
+        cache[1] = max(nums[1], nums[0])
         def memo(i):
-            if i == 0:
-                return nums[0]
-            elif i == 1:
-                return max(nums[1], nums[0])
-            return max(nums[i] + memo(i-2), memo(i-1))
+            if i in cache:
+                return cache[i]
+            cache[i] = max(nums[i] + memo(i-2), memo(i-1))
+            return cache[i]
         
         return memo(len(nums)-1)
