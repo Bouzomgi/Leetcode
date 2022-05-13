fd11020c89b8507a577d4ed2504df1f949015c64
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-12 21:19:00 -0400

    Time: 48 ms (37.23%), Space: 14 MB (21.11%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -12,16 +12,28 @@ class Solution:
 #         return memo(len(nums)-1)
     
         #memoization
-        if len(nums) == 1: 
+#         if len(nums) == 1: 
+#             return nums[0]
+        
+#         cache = {}
+#         cache[0] = nums[0]
+#         cache[1] = max(nums[1], nums[0])
+#         def memo(i):
+#             if i in cache:
+#                 return cache[i]
+#             cache[i] = max(nums[i] + memo(i-2), memo(i-1))
+#             return cache[i]
+        
+#         return memo(len(nums)-1)
+
+        #tabulation
+        if len(nums) == 1:
             return nums[0]
-        
-        cache = {}
-        cache[0] = nums[0]
-        cache[1] = max(nums[1], nums[0])
-        def memo(i):
-            if i in cache:
-                return cache[i]
-            cache[i] = max(nums[i] + memo(i-2), memo(i-1))
-            return cache[i]
-        
-        return memo(len(nums)-1)
+        tab = [0] * len(nums)
+        tab[0] = nums[0]
+        tab[1] = max(nums[0], nums[1])
+        for i in range(2, len(tab)):
+            tab[i] = max(nums[i] + tab[i-2], tab[i-1])
+        return tab[-1]
+    
+    
