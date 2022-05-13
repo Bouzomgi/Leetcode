26f8edc3564587dc3f511873f1e5f50c4734ddd8
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-12 20:11:19 -0400

    Time: 969 ms (44.91%), Space: 28.6 MB (10.08%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -9,19 +9,33 @@ class Solution:
         #memoization
         #We have to keep track of a max value thru the process of finding the max subarray
         #Tricky to do with recursion, so recursive memoization is not a good technique
-        cache = {}
-        cache[0] = nums[0]
-        global maxVal
-        maxVal = cache[0]
-        def memo(i):
-            if i in cache:
-                return cache[i]
-            cache[i] = max(nums[i] + memo(i-1), nums[i])
-            global maxVal
-            if cache[i] > maxVal: 
-                maxVal = cache[i]
-            return cache[i]
+#         cache = {}
+#         cache[0] = nums[0]
+#         global maxSum
+#         maxSum = cache[0]
+#         def memo(i):
+#             if i in cache:
+#                 return cache[i]
+#             cache[i] = max(nums[i] + memo(i-1), nums[i])
+#             global maxSum
+#             if cache[i] > maxSum: 
+#                 maxSum = cache[i]
+#             return cache[i]
     
-        memo(len(nums)-1)
-        return maxVal
+#         memo(len(nums)-1)
+#         return maxSum
+    
+        #tabulation
+        if len(nums) == 1: 
+            return nums[0]
+        
+        tab = [0] * len(nums)
+        tab[0] = nums[0]
+        maxSum = nums[0]
+        for i in range(1, len(nums)):
+            tab[i] = max(nums[i], nums[i]+tab[i-1])
+            if tab[i] > maxSum:
+                maxSum = tab[i]
+        return maxSum
+        
             
