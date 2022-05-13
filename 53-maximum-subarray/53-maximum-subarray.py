07350c164ef20116abb842ae901b77ac4cc4a261
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-12 20:17:45 -0400

    Time: 772 ms (83.91%), Space: 28 MB (40.10%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -25,17 +25,27 @@ class Solution:
 #         memo(len(nums)-1)
 #         return maxSum
     
+    
         #tabulation
-        if len(nums) == 1: 
-            return nums[0]
+#         if len(nums) == 1: 
+#             return nums[0]
         
-        tab = [0] * len(nums)
-        tab[0] = nums[0]
-        maxSum = nums[0]
+#         tab = [0] * len(nums)
+#         tab[0] = nums[0]
+#         maxSum = nums[0]
+#         for i in range(1, len(nums)):
+#             tab[i] = max(nums[i], nums[i]+tab[i-1])
+#             if tab[i] > maxSum:
+#                 maxSum = tab[i]
+#         return maxSum
+    
+        #reduced tab (since we are only using our previous computation in our table to 
+        #generate the next computation, we do not need to store these computations beyond 
+        #the recurrance relationship)
+        maxVal, currMax = nums[0], nums[0]
         for i in range(1, len(nums)):
-            tab[i] = max(nums[i], nums[i]+tab[i-1])
-            if tab[i] > maxSum:
-                maxSum = tab[i]
-        return maxSum
-        
+            currMax = max(currMax + nums[i], nums[i])
+            if currMax > maxVal:
+                maxVal = currMax
+        return maxVal
             
