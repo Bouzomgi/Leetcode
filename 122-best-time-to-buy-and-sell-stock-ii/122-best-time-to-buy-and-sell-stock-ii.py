0224d95f2806f9b73917c35454fc7a0c53a20e8a
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-14 02:49:20 -0400

    Time: 76 ms (62.03%), Space: 15.3 MB (22.89%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        #first attempt solution (not looking at answer)
-        lastdip = prices[0]
-        lastpeak = 0 #we cannot sell on the first day so we do not treat it as a peak
-        prevstock = 0
+        # #first attempt solution (not looking at answer)
+        # lastdip = prices[0]
+        # lastpeak = 0 #we cannot sell on the first day so we do not treat it as a peak
+        # prevstock = 0
+        # maxprof = 0
+        # #want to buy on the dips and sell on the peaks
+        # for i in prices[1:] + [0]:  #add a 0 at the end to add a peak on the last day
+        #     lastpeak = max(lastpeak, i)
+        #     #local max
+        #     if prevstock > i:
+        #         maxprof += (lastpeak - lastdip)
+        #         lastdip, lastpeak = i, i
+        #     else: 
+        #         lastdip = min(lastdip, i)
+        #     prevstock = i
+        # return maxprof
+            
         maxprof = 0
-        #want to buy on the dips and sell on the peaks
-        for i in prices[1:] + [0]:  #add a 0 at the end to add a peak on the last day
-            lastpeak = max(lastpeak, i)
-            
-            #local max
-            if prevstock > i:
-                maxprof += (lastpeak - lastdip)
-                lastdip, lastpeak = i, i
-                
-            else: 
-                lastdip = min(lastdip, i)
-            
-            prevstock = i
-
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                maxprof += prices[i] - prices[i-1]
         return maxprof
-            
-                
