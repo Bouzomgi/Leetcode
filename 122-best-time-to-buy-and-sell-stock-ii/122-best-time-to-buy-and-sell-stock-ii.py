bcd1cb9bd83d4873aa5c01473a08a4a4a4e6bfef
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-13 23:22:15 -0400

    Time: 84 ms (50.40%), Space: 15.3 MB (7.83%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        #first attempt solution (not looking at answer)
         lastdip = prices[0]
-        lastpeak = 0
+        lastpeak = 0 #we cannot sell on the first day so we do not treat it as a peak
         prevstock = 0
         maxprof = 0
         #want to buy on the dips and sell on the peaks
-        for i in prices[1:] + [0]:
+        for i in prices[1:] + [0]:  #add a 0 at the end to add a peak on the last day
             lastpeak = max(lastpeak, i)
             
             #local max
