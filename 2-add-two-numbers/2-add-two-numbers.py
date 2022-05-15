15167a90ecf2cf44d0ee86948f3647d414790150
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-15 01:05:05 -0400

    Time: 79 ms (67.50%), Space: 13.9 MB (87.22%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,27 +5,62 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        total = 0
-        factor = 1
-        while l1 or l2:
-            if l1:
-                total += (l1.val * factor)
-                l1 = l1.next
-            if l2:
-                total += (l2.val * factor)
-                l2 = l2.next
-            factor *= 10
+
+        # first approach
+#         total = 0
+#         factor = 1
+#         while l1 or l2:
+#             if l1:
+#                 total += (l1.val * factor)
+#                 l1 = l1.next
+#             if l2:
+#                 total += (l2.val * factor)
+#                 l2 = l2.next
+#             factor *= 10
         
+#         sumLLHead = ListNode()
+#         sumLL = sumLLHead
+#         sumLL.val = total % 10
+#         total = total // 10 
+        
+#         while total:
+#             sumLL.next = ListNode()
+#             sumLL = sumLL.next
+#             sumLL.val = total % 10
+#             total = total // 10
+            
+#         return sumLLHead
+
+    
+        #second approach
         sumLLHead = ListNode()
         sumLL = sumLLHead
-        sumLL.val = total % 10
-        total = total // 10 
-        
-        while total:
-            sumLL.next = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            currSum = 0
+            if l1:
+                currSum += l1.val
+                l1 = l1.next
+            if l2:
+                currSum += l2.val
+                l2 = l2.next
+                
+            sumLL.next = ListNode((currSum + carry) % 10)
+                
+            if currSum + carry >= 10:
+                carry = 1
+            else:
+                carry = 0
+                
             sumLL = sumLL.next
-            sumLL.val = total % 10
-            total = total // 10
             
-        return sumLLHead
-
+        return sumLLHead.next
+            
+            
+            
+            
+            
+            
+            
+            
+            
