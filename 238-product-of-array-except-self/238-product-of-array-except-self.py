679aaf7e9087e67b18601c8ea78cd475365498c9
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-18 23:28:40 -0400

    Time: 262 ms (68.10%), Space: 21.4 MB (37.91%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -10,23 +10,37 @@ class Solution:
                     
 #         return products
     
-        #O(n) w/o using division
-        forward = [1 for _ in range(len(nums))]
-        backward = [1 for _ in range(len(nums))]
+#         #O(n) w/o using division
+#         forward = [1 for _ in range(len(nums))]
+#         backward = [1 for _ in range(len(nums))]
 
-        prev = 1
-        for i in range(1, len(nums)):
-            forward[i] *= nums[i-1] * forward[i-1]
+#         prev = 1
+#         for i in range(1, len(nums)):
+#             forward[i] *= nums[i-1] * forward[i-1]
             
-            j = len(nums) - i - 1
-            backward[j] *= nums[j+1] * backward[j+1]
+#             j = len(nums) - i - 1
+#             backward[j] *= nums[j+1] * backward[j+1]
             
-        for i in range(len(forward)):
-            forward[i] *= backward[i]
+#         for i in range(len(forward)):
+#             forward[i] *= backward[i]
         
-        return forward
+#         return forward
 
-
+        #O(n) w/o using division using O(1) space
+        storeForward = 1
+        final = [1 for _ in range(len(nums))]
+        
+        for i in range(1, len(nums)):
+            final[i] *= nums[i-1] * final[i-1]            
+        
+        print(final)
+        backwards = 1
+        for i in range(1, len(nums)):
+            j = len(nums) - i - 1
+            backwards *= nums[j+1]
+            final[j] *= backwards
+            
+        return final
             
         
     
