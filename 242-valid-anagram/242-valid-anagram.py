bc9ba50eacec1312ddfa8ca5113b52022938162f
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-05-27 23:07:24 -0400

    Time: 63 ms (59.97%), Space: 14.4 MB (67.14%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,20 +1,37 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counter = {}
+        #Runtime: O(n)
+        #Space: O(n)
+#         counter = {}
+#         for i in s:
+#             if i not in counter:
+#                 counter[i] = 1
+#             else:
+#                 counter[i] += 1
+                
+#         for i in t:
+#             if i not in counter:
+#                 return False
+#             elif counter[i] == 1:
+#                 del counter[i]
+#             else:
+#                 counter[i] -= 1
+            
+#         if counter:
+#             return False
+#         return True
+
+        #Runtime: O(n)
+        #Space: 1
+        counter = [0]*26
         for i in s:
-            if i not in counter:
-                counter[i] = 1
-            else:
-                counter[i] += 1
+            counter[ord(i)%26] += 1
                 
         for i in t:
-            if i not in counter:
-                return False
-            elif counter[i] == 1:
-                del counter[i]
-            else:
-                counter[i] -= 1
+            counter[ord(i)%26] -= 1
             
-        if counter:
-            return False
+        for i in counter:
+            if i != 0:
+                return False
+            
         return True
