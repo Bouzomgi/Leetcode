6efc22fae85eee1a7252f8ed0706e906a7354398
Brian Ouzomgi <43935036+Bouzomgi@users.noreply.github.com>
2022-06-13 22:18:14 -0400

    Time: 163 ms (37.90%), Space: 17.3 MB (80.60%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -11,27 +11,48 @@ class Solution:
                     
             return strCounter
                 
-        
+        #Runtime: O(n^2)
+#         output = []
+#         counterList = []
+#         for elem in strs:
+#             counter = countLetters(elem)
+            
+#             i = 0
+#             while i < len(counterList):
+#                 if counter == counterList[i]:
+#                     output[i].append(elem)
+#                     break
+#                 i += 1
+            
+#             if i == len(counterList):
+#                 output.append([elem])
+#                 counterList.append(counter)
+                
+#         return output
+    
+    
+        #Runtime: O(n)
+        hashList = []
+        hashDict = {}
         output = []
-        counterList = []
+
         for elem in strs:
-            counter = countLetters(elem)
-            
-            i = 0
-            while i < len(counterList):
-                if counter == counterList[i]:
-                    output[i].append(elem)
-                    break
-                i += 1
-            
-            if i == len(counterList):
-                output.append([elem])
-                counterList.append(counter)
+            sortedElem = sorted(elem)
+            hashedElem = hash(tuple(sortedElem))
+            hashList.append(hashedElem)
+        
+        x = 0
+        for i in range(len(strs)):
+            if hashList[i] in hashDict:
+                anagramIndex = hashDict[hashList[i]]
+                output[anagramIndex].append(strs[i])
                 
+            else:
+                hashDict[hashList[i]] = x
+                x += 1
+                output.append([strs[i]])
+            
+
         return output
-
-            
-
-                
                 
         
